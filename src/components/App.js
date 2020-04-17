bd23aa14544ba389835af3154ad408c88c1b4a69
Jean Bien Aime Byiringiro <jeanaime.byiringiro@Jeans-MacBook-Pro-3.local>
2020-04-17 04:10:08 +0200

    ch(sass): setup sass
    - install node-sass and sass-loader dependecies
    - add sass configuration in webpack file
    
    [Maintains #170947488]

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import test from '../redux/actions/test-redux';
+import './app.scss';
 
-function App() {
+const App = () => {
   const testMessage = useSelector((state) => state.test);
   const dispatch = useDispatch();
 
   return (
     <div>
-      <h1>Welcome to Barefoot Nomad</h1>
+      <h1 className="welcome">Welcome to Barefoot Nomad</h1>
       <h3>{testMessage}</h3>
       <button type="submit" onClick={() => dispatch(test())}>Test Redux</button>
     </div>
   );
-}
+};
 export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -43,6 +43,10 @@ module.exports = {
         use: ['babel-loader', 'eslint-loader'],
       },
       {
+        test: /(\.scss)$/,
+        use: ['style-loader', 'css-loader','sass-loader'],
+      },
+      {
         test: /(\.css)$/,
         use: ['style-loader', 'css-loader'],
       },
