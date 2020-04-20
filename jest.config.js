ab7f50e9ab9e8472c333bc4d63d29aaf6cbe24d4
Baraka-Mugisha <mugishaje@gmail.com>
2020-04-20 11:14:36 +0200

    chore(testsSettings): Set up testing environment
    installs jest
    installs enzyme
    adds configurations for jest and enzyme
    adds a sample test
    
    [Maintains #170947485]

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  clearMocks: true,
+
+  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+
+  coverageDirectory: 'coverage',
+
+  moduleFileExtensions: ['js', 'json', 'jsx', 'scss'],
+
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js"
+  },
+
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+
+  testEnvironment: 'jsdom',
+
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+
+  testURL: 'http://localhost',
+
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  verbose: false,
+};
